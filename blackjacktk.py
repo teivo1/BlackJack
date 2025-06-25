cd7b3e797fc7f9201cfbc410c1a68f2a1081484b
Alexey <belevancev032@gmail.com>
2025-06-25 15:08:21 +0300

    Update blackjacktk.py

--- blackjacktk.py
+++ blackjacktk.py
@@ -2,7 +2,6 @@ from tkinter import *
 from random import choice
 from PIL import Image, ImageTk
 
-#Подсчёт карт
 def card_score(card):
     if card in ["J", "Q", "K"]:
         return 10
@@ -11,24 +10,20 @@ def card_score(card):
     else:
         return int(card)
 
-#Генерация случайной карты
 def generate_card():
     card = choice(card_list)
     suit = choice(suit_list)
     return card, suit
 
-#Загрузка изображения карты
 def load_card_image(path):
     image = Image.open(path).convert("RGBA")
     return ImageTk.PhotoImage(image)
 
-#Кнопка "Play Again"
 def show_play_again():
     btn_hit.place_forget()
     btn_stand.place_forget()
     btn_play_again.place(x=385, y=560)
 
-#Функция для кнопки "Play Again"
 def reset_game():
     global place_player, place_dealer
 
@@ -50,7 +45,6 @@ def reset_game():
     btn_stand.place(x=455, y=560)
     btn_play_again.place_forget()
 
-    #Карты дилера
     img_back = load_card_image('cards/cardback.png')
     lbl_back = Label(root, image=img_back, bg='#35654d')
     lbl_back.place(x=409, y=20)
@@ -68,7 +62,6 @@ def reset_game():
     sum_dealer.append(score)
     score_label_d.config(text=f"Dealer: {sum(sum_dealer)}")
 
-    #Карты игрока
     for i in range(2):
         card, suit = generate_card()
         img = load_card_image(f'cards/{card}-{suit}.png')
@@ -94,7 +87,6 @@ card_labels = []
 place_player = 471
 place_dealer = 471
 
-#Кнопка "Hit"
 def hit():
     global place_player
     card, suit = generate_card()
@@ -120,7 +112,6 @@ def hit():
         card_labels.append(total)
         show_play_again()
 
-#Кнопка "Stand"
 def stand():
     btn_hit['state'] = 'disabled'
     btn_stand['state'] = 'disabled'
@@ -186,7 +177,6 @@ def result():
 
     show_play_again()
 
-#Колода
 suit_list = ['C', 'D', 'H', 'S']
 card_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 sum_player = []
@@ -199,7 +189,6 @@ root.geometry('880x600+520+240')
 root.config(bg='#35654d')
 root.resizable(width=False, height=False)
 
-#Кнопки
 btn_hit = Button(root, text='Hit', font=('Arial', 13, 'bold'), width=5, command=hit)
 btn_hit.place(x=365, y=560)
 
@@ -208,7 +197,6 @@ btn_stand.place(x=455, y=560)
 
 btn_play_again = Button(root, text='Play again', font=('Arial', 13, 'bold'), width=10, command=reset_game)
 
-#Карты дилера
 img_back = load_card_image('cards/cardback.png')
 lbl_back = Label(root, image=img_back, bg='#35654d')
 lbl_back.place(x=409, y=20)
@@ -224,11 +212,9 @@ dealer_images.append(img)
 score = card_score(card)
 sum_dealer.append(score)
 
-#Счёт дилера
 score_label_d = Label(root, text=f"Dealer: {sum(sum_dealer)}", font=('Arial', 18, 'bold'), bg='#35654d', fg='white')
 score_label_d.place(x=115, y=100)
 
-#Карты игрока
 place_player = 347
 for i in range(2):
     card, suit = generate_card()
@@ -245,7 +231,6 @@ if sum(sum_player) == 22:
     sum_player.remove(11)
     sum_player.append(1)
 
-#Счёт игрока
 score_label = Label(root, text=f"Player: {sum(sum_player)}", font=('Arial', 18, 'bold'), bg='#35654d', fg='white')
 score_label.place(x=115, y=400)
 
